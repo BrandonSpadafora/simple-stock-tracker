edb5af91722d46787af93f4d6b0537a122afb9d1
Brandon Spadafora <brandon.spadafora01@gmail.com>
2022-03-02 22:45:08 -0500

    Update stock_tracker.py

--- stock_trading_app/stock_tracker.py
+++ stock_trading_app/stock_tracker.py
@@ -8,12 +8,14 @@ Shown are the stock **closing price** and ***volume*** of Microsoft!
 
 #define the ticker symbol
 tickerSymbol = 'MSFT'
+
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
+
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='max')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+#headings for charts
 st.write("""
 ## Closing Price
 """)
